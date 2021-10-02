acfaf0dbfd33afe556655c994665da3e20e42edb
AweAwe95 <vitalikarach95@gmail.com>
2021-10-02 22:08:22 +0300

    20-23 - add Dialog and Message(props for Dialogs)

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -1,11 +1,13 @@
 import p from "../Profile.module.css";
 
 export function ProfileInfo() {
-    return <>
-        <div>
-            <img src="https://gamemag.ru/images/cache/Reviews/Reviews1726/92f0dfdd74-2_1390x600.jpg"
-                 className={p.profilePageImg} alt=""/>
-        </div>
-        <div>Avatar + description</div>
-    </>;
+    return (
+        <>
+            <div>
+                <img src="https://gamemag.ru/images/cache/Reviews/Reviews1726/92f0dfdd74-2_1390x600.jpg"
+                     className={p.profilePageImg} alt=""/>
+            </div>
+            <div>Avatar + description</div>
+        </>
+    )
 }
